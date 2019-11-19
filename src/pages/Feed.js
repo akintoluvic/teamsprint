e0c83a5c4c61df5f1a1c8f806c1dc0b360b12fae
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-19 19:34:52 +0100

    init

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -6,7 +6,14 @@ export default function Feed() {
     return (
         <div>
             <Navbar />
+            <div className='page-container'>
+                <div className='sidebar'>
 
+                </div>
+                <div className='main-feed'>
+
+                </div>
+            </div>
         </div>
     )
 }
