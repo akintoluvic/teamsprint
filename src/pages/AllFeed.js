05e10554ff82ceb2949c526e4e72b29bd1794372
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-21 18:24:10 +0100

    init

--- src/pages/AllFeed.js
+++ src/pages/AllFeed.js
@@ -6,24 +6,42 @@ import ProfileForm from "../components/ProfileForm";
 import Feed from "../partials/Feed";
 import EachPost from "../partials/EachPost";
 import Post from "../partials/Post";
+import {
+    Switch,
+    Route,
+    Redirect,
+    useHistory,
+    useLocation
+  } from "react-router-dom";
+  
 
 export default function AllFeed() {
     return (
-        <div>
-            <Navbar />
-            <div className='page-container'>
-                <div className='sidebar'>
-                    <SmallProfile />
-                    <SidebarTags />
-                    <p>©2019 Greene Teamwork. All rights reserved.</p>
-                </div>
-                <div className='main-feed'>
-                    <ProfileForm />
-                    {/* <Feed /> */}
-                    {/* <EachPost /> */}
-                    {/* <Post /> */}
-                </div>
-            </div>
+      <div>
+        <Navbar />
+        <div className="page-container">
+          <div className="sidebar">
+            <SmallProfile />
+            <SidebarTags />
+            <p>©2019 Greene Teamwork. All rights reserved.</p>
+          </div>
+          <div className="main-feed">
+            <Switch>
+              <Route path="/feed">
+                <Feed />
+              </Route>
+              <Route path="/create-user">
+                <ProfileForm />
+              </Route>
+              <Route path="/post">
+                <Post />
+              </Route>
+              <Route path="/a-post">
+                <EachPost />
+              </Route>
+            </Switch>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
