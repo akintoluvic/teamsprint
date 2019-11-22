dea4729d0a2bc81597ef3a65de00bedeb04ddf3a
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-22 15:34:01 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,45 +1,27 @@
-import React from 'react';
+import React from "react";
 // import AuthExample from './testPAge';
 import Home from "./pages/Home";
 import AllFeed from "./pages/AllFeed";
-import ProfileForm from "./components/ProfileForm";
-import Feed from "./pages/Feed";
-import EachPost from "./pages/EachPost";
-import Post from "./partials/Post";
-import CreatePost from "./pages/CreatePost";
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
+  Route
   // Link,
   // Redirect,
   // useHistory,
   // useLocation
 } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Switch>
-            <Route exact path="/" component={Home}/>
-              <Route exact path="/feed">
-                <Feed />
-              </Route>
-              <Route exact path="/create-user">
-                <ProfileForm />
-              </Route>
-              <Route exact path="/post">
-                <CreatePost />
-              </Route>
-              <Route exact path="/a-post">
-                <EachPost />
-              </Route>
-              <Route component={Error} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={AllFeed} />
         </Switch>
-
 
         {/* { isUserAuthenticated() ? props.children : <Redirect to={routes.login} /> } */}
       </div>
--- src/pages/AllFeed.js
+++ src/pages/AllFeed.js
@@ -1,47 +1,48 @@
-// import React from 'react';
-// import Navbar from "../components/Navbar";
-// import SmallProfile from "../components/SmallProfile";
-// import SidebarTags from "../components/SidebarTags";
-// import ProfileForm from "../components/ProfileForm";
-// import Feed from "../partials/Feed";
-// import EachPost from "../partials/EachPost";
-// import Post from "../partials/Post";
-// import {
-//     Switch,
-//     Route,
-//     Redirect,
-//     useHistory,
-//     useLocation
-//   } from "react-router-dom";
+import React from 'react';
+import Navbar from "../components/Navbar";
+import SmallProfile from "../components/SmallProfile";
+import SidebarTags from "../components/SidebarTags";
+import ProfileForm from "../components/ProfileForm";
+import Feed from "./Feed";
+import EachPost from "./EachPost";
+import Post from "../partials/Post";
+import {
+    Switch,
+    Route,
+    Redirect,
+    useHistory,
+    useLocation
+  } from "react-router-dom";
   
 
-// export default function AllFeed() {
-//     return (
-//       <div>
-//         <Navbar />
-//         <div className="page-container">
-//           <div className="sidebar">
-//             <SmallProfile />
-//             <SidebarTags />
-//             <p>©2019 Greene Teamwork. All rights reserved.</p>
-//           </div>
-//           <div className="main-feed">
-//             <Switch>
-//               <Route path="/feed">
-//                 <Feed />
-//               </Route>
-//               <Route path="/create-user">
-//                 <ProfileForm />
-//               </Route>
-//               <Route path="/post">
-//                 <Post />
-//               </Route>
-//               <Route path="/a-post">
-//                 <EachPost />
-//               </Route>
-//             </Switch>
-//           </div>
-//         </div>
-//       </div>
-//     );
-// }
+export default function AllFeed() {
+    return (
+      <div>
+        <Navbar />
+        <div className="page-container">
+          <div className="sidebar">
+            <SmallProfile />
+            <SidebarTags />
+            <p>©2019 Greene Teamwork. All rights reserved.</p>
+          </div>
+          <div className="main-feed">
+            <Switch>
+              <Route path="/feed">
+                <Feed />
+              </Route>
+              <Route path="/create-user">
+                <ProfileForm />
+              </Route>
+              <Route path="/post">
+                <Post />
+              </Route>
+              <Route path="/a-post">
+                <EachPost />
+              </Route>
+              <Route component={Error} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+}
