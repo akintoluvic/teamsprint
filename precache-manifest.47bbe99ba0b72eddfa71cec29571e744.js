a7750bb03878715b127111eebe7168bbc7d88e2d
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-01-09 23:54:00 +0100

    Updates

---
+++ precache-manifest.47bbe99ba0b72eddfa71cec29571e744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1ab6cb0958f0a46b11d1875ba327b1",
+    "url": "/teamsprint/index.html"
+  },
+  {
+    "revision": "3682b7fb0f9f35a6721e",
+    "url": "/teamsprint/static/css/main.42850750.chunk.css"
+  },
+  {
+    "revision": "40826f18f44c283e648c",
+    "url": "/teamsprint/static/js/2.6e6d0bf7.chunk.js"
+  },
+  {
+    "revision": "3682b7fb0f9f35a6721e",
+    "url": "/teamsprint/static/js/main.2cf86cec.chunk.js"
+  },
+  {
+    "revision": "571e1a58cc4056fd32da",
+    "url": "/teamsprint/static/js/runtime-main.f9e1c105.js"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "/teamsprint/static/media/avatar.e67e358c.jpg"
+  },
+  {
+    "revision": "598ed987b6bdfa657c9133b007c5881e",
+    "url": "/teamsprint/static/media/logo.598ed987.svg"
+  }
+]);
